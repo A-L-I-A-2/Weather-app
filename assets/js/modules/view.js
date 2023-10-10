b89315def7e8aec3882670679364fe701eaf2984
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-10-10 10:48:13 +0200

    Data refactored - now usable

---
+++ assets/js/modules/view.js
@@ -0,0 +1,37 @@
+export function DisplayTemperature(currentTempData, displayElement) {
+
+    console.log(currentTempData.tempData.temperature);
+    
+
+    // find dom elemenent
+    const myApp = document.getElementById(displayElement);
+
+    myApp.innerHTML = "";
+
+    let myTempHtml = "";
+    myTempHtml =`<h2>Temperature: ${currentTempData.tempData.temperature} °C</h2>`;
+
+    myApp.innerHTML = myTempHtml;
+} 
+
+export function DisplayWeatherType(currentWeatherType, displayElement) {
+    console.log(currentWeatherType.weatherType[0]);
+    const myApp = document.getElementById(displayElement);
+
+    myApp.innerHTML = "";
+
+    let myWeatherTypeHtml = "";
+
+    myWeatherTypeHtml = `<i>${currentWeatherType.weatherType[0]}</i>`;
+
+    myApp.innerHTML = myWeatherTypeHtml;
+}
+
+export function DisplaySunset(currentSunset, displayElement) {
+    console.log(currentSunset.sunData[0]);
+    
+}
+
+export function DisplayWind(currentWindData, displayElement) {
+    console.log(currentWindData.windData);
+}
