4d73977057a1b0f73a0a4f7919b6ce3835266b09
dkanders12 <dkanders12@gmail.com>
2023-10-11 12:21:46 +0200

    sunset display

--- assets/js/modules/view.js
+++ assets/js/modules/view.js
@@ -1,39 +1,57 @@
 export function DisplayTemperature(currentTempData, displayElement) {
+  // find dom elemenent
+  const myApp = document.getElementById(displayElement);
 
-    console.log(currentTempData.tempData.temperature);
-    
+  myApp.innerHTML = "";
 
-    // find dom elemenent
-    const myApp = document.getElementById(displayElement);
+  let myTempHtml = "";
+  myTempHtml = `<h2>${currentTempData.tempData.temperature} °C</h2>`;
 
-    myApp.innerHTML = "";
+  myApp.innerHTML = myTempHtml;
+}
+export function DisplayWind(currentWindData, displayElement) {
+  const Mywind = document.getElementById(displayElement);
 
-    let myTempHtml = "";
-    myTempHtml =`<h2>Temperature: ${currentTempData.tempData.temperature} °C</h2>`;
-
-    myApp.innerHTML = myTempHtml;
-} 
+  let windSpeedHTML = `<h2>${currentWindData.windData.windSpeed}</h2>`;
+  let windDiretionHTML = `<h2>${currentWindData.windData.windDirection}</h2>`;
+  Mywind.innerHTML = windSpeedHTML + windDiretionHTML;
+}
 
 export function DisplayWeatherType(currentWeatherType, displayElement) {
-    console.log(currentWeatherType.weatherType[0]);
-    const myApp = document.getElementById(displayElement);
+  const myApp = document.getElementById(displayElement);
 
-    myApp.innerHTML = "";
+  myApp.innerHTML = "";
 
-    let myWeatherTypeHtml = "";
+  let myWeatherTypeHtml = "";
 
-    myWeatherTypeHtml = `<i>${currentWeatherType.weatherType[0]}</i>`;
+  myWeatherTypeHtml = `<i>${currentWeatherType.weatherType[0]}</i>`;
 
-    myApp.innerHTML = myWeatherTypeHtml;
+  myApp.innerHTML = myWeatherTypeHtml;
 }
 
 export function DisplaySunset(currentSunset, displayElement) {
-    console.log(currentSunset.sunData[0]);
-    
-}
+  const MYSunSet = document.getElementById(displayElement);
 
-export function DisplayWind(currentWindData, displayElement) {
-    console.log(currentWindData.windData);
-
-    const myApp = document.getElementById(displayElement);
+  let sunRiseHTML = `<h2>${currentSunset.sunData[0].sunrise}</h2>`;
+  let sunSetHTML = `<h3>${currentSunset.sunData[0].sunset}</h3>`;
+  MYSunSet.innerHTML = sunRiseHTML + sunSetHTML;
 }
+const yeet = document.querySelectorAll(".box1");
+
+yeet.forEach((element) => {
+  const weatherText = element.querySelector(".weather-text");
+
+  // Hide the text when the page loads
+  weatherText.style.display = "none"; // Hide the text
+
+  element.addEventListener("mousedown", () => {
+    // Toggle the "origin" class on click
+    element.classList.toggle("origin");
+
+    if (element.classList.contains("origin")) {
+      weatherText.style.display = "block"; // Show the text
+    } else {
+      weatherText.style.display = "none"; // Hide the text
+    }
+  });
+});
